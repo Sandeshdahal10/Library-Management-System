786c3106d8c902b817f906fa74addffcc47317fe
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-07-17 13:11:16 +0545

    RBAC system for library management System

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./database/database.js";
+import router from "./routes/book.route.js";
+import borrowRouter from "./routes/borrower.route.js";
+import authRouter from "./routes/auth.route.js";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+connectDB();
+
+app.use("/api", router);
+app.use("/api", borrowRouter);
+app.use("/api", authRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
