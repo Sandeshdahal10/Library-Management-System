98bfe919e1217b5b44e49b9f556d9faa6bbef5b5
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-08-07 19:51:44 +0545

    fix cors configuration in server.js

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ import connectDB from "./database/database.js";
 import router from "./routes/book.route.js";
 import borrowRouter from "./routes/borrower.route.js";
 import authRouter from "./routes/auth.route.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true,
+}));
 
 const PORT = process.env.PORT || 8000;
 connectDB();
