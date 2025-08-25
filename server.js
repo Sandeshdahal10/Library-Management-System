e4ccc88e4879d4657a1ee6a93fb07b98229cd965
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-08-25 17:37:17 +0545

    making backend public

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use("/api", router);
 app.use("/api", borrowRouter);
 app.use("/api", authRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
